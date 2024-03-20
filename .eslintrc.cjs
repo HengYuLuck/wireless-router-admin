e9fedac9096246d7ca75ca288b1eec6156d0b734
HengYu <hengyu@ailinking.com>
2024-03-20 10:08:53 +0800

    feat: 增加header 组件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,48 +1,23 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    'extends': [
+    extends: [
         'eslint:recommended',
+        'plugin:vue/vue3-essential',
         'plugin:@typescript-eslint/recommended',
-        'plugin:vue/vue3-essential'
+        '@antfu',
     ],
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'parser': '@typescript-eslint/parser',
-        'sourceType': 'module'
+    parserOptions: {
+        ecmaVersion: 'latest',
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
     },
-    'plugins': [
-        '@typescript-eslint',
-        'vue'
-    ],
-    'rules': {
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        'no-multi-spaces': 'error' // 添加 no-multi-spaces 规则并将其设置为 error
-    }
-};
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {
+
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/multiline-html-element-content-newline': 'off',
+    },
+}
