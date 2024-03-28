0ec4356ef3be689ac28e50d3118c059a06e4ad06
HengYu <hengyu@ailinking.com>
2024-03-28 15:04:59 +0800

    feat: 封装use button 组件，测试透传Attributes

---
+++ src/components/UseButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {defineProps, onMounted} from 'vue';
+import {useAttrs} from "vue";
+defineOptions({ // 默认不生效
+  inheritAttrs: false
+})
+const props = defineProps({
+  defaultBtnClass: {
+    type: String,
+    default: 'tw-border tw-p-1 tw-px-3 tw-text-xs  tw-rounded hover:tw-bg-sky-500/100 ',
+  },
+})
+const $attrs = useAttrs()
+onMounted(()=>{
+  console.log($attrs);
+})
+
+</script>
+
+<template>
+<!--  <el-button v-bind="$attrs" :style="{margin : defaultMargin + 'px', height: defaultHeight + 'px'}">Default</el-button>-->
+<div>
+  测试
+  <button v-bind="$attrs" :class="defaultBtnClass">test</button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,12 +1,14 @@
 <script setup>
 
 import Header from '../components/Header.vue';
+import UseButton from "../components/UseButton.vue";
 </script>
 
 <template>
   <div class="tw-w-full tw-h-full">
     <Header></Header>
     <router-view />
+    <UseButton class="tw-bg-red-500 tw-text-green-500 tw-px-10"></UseButton>
   </div>
 
 </template>
